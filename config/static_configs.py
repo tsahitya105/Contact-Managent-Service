18ab0fd7cabfd29e6afd5d6d92e88fbc98a2dca8
SAHITYA TAHILIANI <tsahitya105@gmail.com>
2023-08-09 18:45:42 +0530

    Update static_configs.py

--- config/static_configs.py
+++ config/static_configs.py
@@ -1,3 +1,4 @@
 class StaticConfigs:
-    DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@db:5432/postgres'
+    DATABASE_URI = 'postgresql+psycopg2://tsahitya105:j5MBbGFKRYNrhLfudeDK3KDjseTj0Qeu@dpg-cj9okkivvtos73868p9g-a/bitespeed_zb07'
+    # DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@db:5432/postgres'
     # libs//user:pass@host:port/db_name
